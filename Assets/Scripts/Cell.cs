156cbf05e2c2b1289d22c30279549a8f21712d6a
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-07-15 03:20:28 -0400

    Initial commit

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Cell", menuName ="Cell")]
+public class Cell : ScriptableObject
+{
+    public Sprite sprite;
+    public enum Type
+    {
+        PLAYER,
+        ENEMY,
+        ITEM
+    }
+    public Type type;
+    public List<string> rpsList;
+}
